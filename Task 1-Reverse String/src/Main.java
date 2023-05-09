1d6a63d7faa6557061aaf1929ff4fbe9f3d66657
Amira20208045 <amira20208045@fci.helwan.edu.eg>
2023-05-09 19:45:54 +0300

    reverse string using StringBuilder added

--- Task 1-Reverse String/src/Main.java
+++ Task 1-Reverse String/src/Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class Main {
 
+    // time complexity : O(n)
+    // space complexity : O(n)
     static String reverseString(String s){
         char []chars = s.toCharArray();
         int begin = 0 , end = s.length()-1;
@@ -15,9 +17,25 @@ public class Main {
         }
         return new String(chars);
     }
+
+    // time complexity : O(n)
+    // space complexity : O(n)
+    static String reverseStringBuilder(String s){
+       StringBuilder sb = new StringBuilder();
+       for(int i=s.length()-1; i>=0; i--){
+           sb.append(s.charAt(i));
+       }
+       return sb.toString();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
+
+        // use String class
         System.out.println(reverseString(str));
+
+        // use StringBuilder class
+        System.out.println(reverseStringBuilder(str));
     }
 }
